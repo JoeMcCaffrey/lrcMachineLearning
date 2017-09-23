56ffd2fdd6b126e565a2fb518ab3b56ff3f5b52b
joe <Joe@joe>
2017-09-22 23:34:59 -0400

    adding spam link

--- SpamFilter.py
+++ SpamFilter.py
@@ -42,12 +42,29 @@ cl = NaiveBayesClassifier(train_links)
 test = "D.C. court rules tracking phones without a warrant is unconstitutional"
 
 res = cl.classify(test)
+print "text: ", test
+print ""
 print "The text is Spam: ", res
 
 res_prob = cl.prob_classify(test)
 
 print "Percentage of pos result: ", round(res_prob.prob("pos"), 2)
 print "Percentage of neg result: ", round(res_prob.prob("neg"), 2)
+
+test2 = "11 life-changing trips to take in your 30s"
+print "text: ", test2
+print ""
+
+res2 = cl.classify(test2)
+print "The text is Spam: ", res2
+
+res_prob2 = cl.prob_classify(test2)
+
+print "Percentage of pos result: ", round(res_prob2.prob("pos"), 2)
+print "Percentage of neg result: ", round(res_prob2.prob("neg"), 2)
+
+
+
 
 print cl.show_informative_features(5)
 
